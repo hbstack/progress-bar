ddc71d564798913dd67ef786bbbca6aac9e309fa
razonyang <razonyang@gmail.com>
2024-06-29 10:40:11 +0800

    fix: hide the progress bar on the case of backing forward

--- assets/hb/modules/progress-bar/js/index.ts
+++ assets/hb/modules/progress-bar/js/index.ts
@@ -3,6 +3,10 @@ import Progress from "./progress";
 (() => {
     const progress = new Progress()
 
+    window.addEventListener('pagehide', () => {
+        progress.hide()
+    })
+
     window.addEventListener('beforeunload', () => {
         progress.show()
     })
